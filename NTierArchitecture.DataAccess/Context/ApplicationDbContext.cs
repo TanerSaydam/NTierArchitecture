552f70465ac1ca95ee890e9a463a7a6b5deacaeb
tanersaydam <tanersaydam@gmail.com>
2023-11-18 02:57:05 +0300

    DataAccess Katmanını Oluşturalım ve Db Context Yapımızı Kuralım

---
+++ NTierArchitecture.DataAccess/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NTierArchitecture.Entities.Models;
+
+namespace NTierArchitecture.DataAccess.Context;
+internal sealed class ApplicationDbContext : IdentityDbContext<AppUser, AppRole, Guid>
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+                
+    }
+}
