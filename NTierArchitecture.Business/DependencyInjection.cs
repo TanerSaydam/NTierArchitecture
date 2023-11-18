09e49d45c591a0d6d4c323df537344c2f1d17b8c
tanersaydam <tanersaydam@gmail.com>
2023-11-18 04:08:01 +0300

    Business Katmanını ve Dependency Injection'ı Oluşturalım

---
+++ NTierArchitecture.Business/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace NTierArchitecture.Business;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddBusiness(
+        this IServiceCollection services)
+    {
+        return services;
+    }
+}
