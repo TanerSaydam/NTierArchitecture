934bc46a52c29de7905fbbc171ed16daf8a3d1bf
tanersaydam <tanersaydam@gmail.com>
2023-11-18 05:38:28 +0300

    WebApi Katmanını Oluşturup Diğer Katmanları Bağlayalım

---
+++ NTierArchitecture.WebApi/Program.cs
@@ -0,0 +1,27 @@
+using NTierArchitecture.Business;
+using NTierArchitecture.DataAccess;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddBusiness();
+builder.Services.AddDataAccess(builder.Configuration);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
