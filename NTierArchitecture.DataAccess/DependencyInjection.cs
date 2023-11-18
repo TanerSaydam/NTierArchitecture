ec41a5137e5dc6ff493b395b2e479caa826d7393
tanersaydam <tanersaydam@gmail.com>
2023-11-18 03:46:37 +0300

    Dependency Injection Yapalım

---
+++ NTierArchitecture.DataAccess/DependencyInjection.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NTierArchitecture.DataAccess.Context;
+using NTierArchitecture.DataAccess.Repositories;
+using NTierArchitecture.Entities.Models;
+using NTierArchitecture.Entities.Repositories;
+
+namespace NTierArchitecture.DataAccess;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDataAccess(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        string connectionString = configuration.GetConnectionString("SqlServer");
+        services.AddDbContext<ApplicationDbContext>(opt =>
+        {
+            opt.UseSqlServer(connectionString);
+        });
+        services.AddIdentityCore<AppUser>(cfr=>
+        {
+            cfr.Password.RequireNonAlphanumeric = false;
+        }).AddEntityFrameworkStores<ApplicationDbContext>();
+
+        services.AddScoped<IUnitOfWork>(sv=> sv.GetRequiredService<ApplicationDbContext>());
+
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IUserRoleRepository, UserRoleRepository>();
+
+        return services;
+    }
+}
